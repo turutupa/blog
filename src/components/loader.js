3ad5b209ac6a64645d0f76750b452cee83ba9dda
Turutupa <adc.batros@gmail.com>
2020-10-23 10:59:07 +0200

    window.sessionStorage fix

--- src/components/loader.js
+++ src/components/loader.js
@@ -4,6 +4,7 @@ import styled from "styled-components"
 
 const Container = styled.div`
   display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   height: 100vh;
