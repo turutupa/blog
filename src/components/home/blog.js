3b6e1d18ff1b0ca945abba6eb1503ac1c4c0217d
Turutupa <adc.batros@gmail.com>
2020-12-21 17:55:35 +0100

    first commit

--- src/components/home/blog.js
+++ src/components/home/blog.js
@@ -1,45 +1,62 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import styled from "styled-components"
+
 import { rhythm } from "../../utils/typography"
 import "@deckdeckgo/highlight-code"
+
+const Container = styled.div`
+  margin: 50px 0 40px;
+`
+
+const PostContainer = styled.div`
+  padding: 1px 20px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  box-shadow: 0px 3px 6px rgb(30, 30, 30, 0);
+
+  &:hover {
+    box-shadow: 0px 3px 6px rgb(30, 30, 30, 0.2);
+  }
+`
+
+const H1 = styled.h1`
+  font-size: ${rhythm(0.8)};
+  margin-bottom: ${rhythm(1 / 5)};
+  margin-top: 30px;
+`
 
 function Blog(props) {
   const { data } = props
   const posts = data.allMdx.edges
 
   return (
-    <>
-      <div style={{ margin: "20px 0 40px" }}>
+    <Container>
+      <>
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
-            <div key={node.fields.slug}>
-              <h1
-                style={{
-                  fontSize: rhythm(0.8),
-                  marginBottom: rhythm(1 / 5),
-                  fontFamily: `Merriweather`,
-                }}
-              >
-                <Link
-                  style={{ boxShadow: `none` }}
-                  to={`/blog${node.fields.slug}`}
-                >
-                  {title}
-                </Link>
-              </h1>
-              <small className="blog-date">{node.frontmatter.date}</small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </div>
+            <Link
+              style={{ boxShadow: `none` }}
+              to={`/blog${node.fields.slug}`}
+              key={node.fields.slug}
+            >
+              <PostContainer className="blog-description-container">
+                <H1>{title}</H1>
+                <small className="blog-date">{node.frontmatter.date}</small>
+                <p
+                  className="blog-description"
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </PostContainer>
+            </Link>
           )
         })}
-      </div>
-    </>
+      </>
+    </Container>
   )
 }
 
